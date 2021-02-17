047cbc7eca510ddeeb468c7780fdc4fb911f2434
Shagun Jain <Jshagun710@gmail.com>
2021-02-17 13:03:29 +0530

    update settings.py

--- MLchef/settings.py
+++ MLchef/settings.py
@@ -139,7 +139,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles", "static-root")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
@@ -147,4 +147,4 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 MEDIA_URL = "/media/"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media", "media-root")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
